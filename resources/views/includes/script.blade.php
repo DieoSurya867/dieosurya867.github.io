2c31483518f61f99667a1b87111519ecd3129686
dieosurya867 <dieosurya@gmail.com>
2022-10-16 23:43:39 +0700

    update source UI Store & Component Blade Store

---
+++ resources/views/includes/script.blade.php
@@ -0,0 +1,7 @@
+      <!-- JavaScript files-->
+      <script src={{ asset ("store/vendor/bootstrap/js/bootstrap.bundle.min.js")}}></script>
+      <script src={{ asset ("store/vendor/glightbox/js/glightbox.min.js")}}></script>
+      <script src={{ asset ("store/vendor/nouislider/nouislider.min.js")}}></script>
+      <script src={{ asset ("store/vendor/swiper/swiper-bundle.min.js")}}></script>
+      <script src={{ asset ("store/vendor/choices.js/public/assets/scripts/choices.min.js")}}></script>
+      <script src={{ asset ("store/js/front.js")}}></script>
