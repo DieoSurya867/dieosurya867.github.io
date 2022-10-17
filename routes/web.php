b6a5d27826bf9790ceef232ba11742293ade8692
dieosurya867 <dieosurya@gmail.com>
2022-10-17 22:24:08 +0700

    update kernel & middleware admin

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,6 @@ Route::get('/', function () {
     return view('pages.user.index');
 });
 
-
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
