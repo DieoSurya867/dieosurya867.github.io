23b17ce99292ff09fa8373ccce4422522ff36abb
Brianmarco14 <brianmarco1996@gmail.com>
2022-11-09 18:34:33 +0800

    app

--- public/js/app.js
+++ public/js/app.js
@@ -4,8 +4,51 @@ $(document).ready(function() {
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
         }
     });
-
-
+    // API WILAYAH FUNCTION
+    $.ajax({
+        type: "get",
+        url: "/wilayah",
+        dataType: "json",
+        success: function (response) {
+            response.map((value) => {
+                $('#provinces').append($('<option>', {
+                    value: value.id,
+                    text: value.name
+                }));
+            })
+        }
+    });
+    
+    // const daerah = ()=>{}
+    
+    function daerah(jenis, id) {
+        let dr
+    
+        if (jenis == 'provinces') {
+            dr = 'regencies'
+        }else if (jenis == 'regencies') {
+            dr = 'districts'
+        }else if (jenis == 'districts') {
+            dr = 'villages'
+        }
+        $.ajax({
+            type: "get",
+            url: `https://www.emsifa.com/api-wilayah-indonesia/api/${dr}/${id}.json`,
+            dataType: "json",
+            success: function (response) {
+                console.log(response);
+                $(`#${dr}`).children().remove()
+                response.map((value) => {
+                    $(`#${dr}`).append($('<option>', {
+                        value: value.id,
+                        text: value.name
+                    }));
+                })
+            }
+        });
+    }
+                 
+});
 
     // ADD TO CART FUNCTION
     $('.addToCart').click(function (e) { 
@@ -33,54 +76,3 @@ $(document).ready(function() {
     });
 
 
-
-    // API WILAYAH FUNCTION
-    $.ajax({
-    type: "get",
-    url: "/wilayah",
-    dataType: "json",
-    success: function (response) {
-        response.map((value) => {
-            $('#provinces').append($('<option>', {
-                value: value.id,
-                text: value.name
-            }));
-        })
-    }
-});
-
-// const daerah = ()=>{}
-
-function daerah(jenis, id) {
-    let dr
-
-    if (jenis == 'provinces') {
-        dr = 'regencies'
-    }else if (jenis == 'regencies') {
-        dr = 'districts'
-    }else if (jenis == 'districts') {
-        dr = 'villages'
-    }
-    $.ajax({
-        type: "get",
-        url: `https://www.emsifa.com/api-wilayah-indonesia/api/${dr}/${id}.json`,
-        dataType: "json",
-        success: function (response) {
-            console.log(response);
-            $(`#${dr}`).children().remove()
-            response.map((value) => {
-                $(`#${dr}`).append($('<option>', {
-                    value: value.id,
-                    text: value.name
-                }));
-            })
-        }
-    });
-}
-
-
-
-
-
-
-});
